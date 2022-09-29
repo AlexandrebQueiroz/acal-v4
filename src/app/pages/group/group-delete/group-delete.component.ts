b731ae217a715c3a21f4de79b1e592c50fc79119
Alexandre Queiroz <megalexandre@gmail.com>
2022-09-29 09:44:30 -0300

    edit group

--- src/app/pages/group/group-delete/group-delete.component.ts
+++ src/app/pages/group/group-delete/group-delete.component.ts
@@ -1,10 +1,8 @@
-import { DataService } from './../../../@data/data.service';
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NbToastrService } from '@nebular/theme';
-import { filter } from 'rxjs/operators';
 import { GroupService } from '../group.service';
+import { DataService } from './../../../@data/data.service';
 
 @Component({
   selector: 'ngx-group-delete',
@@ -16,7 +14,6 @@ export class GroupDeleteComponent implements OnInit {
 
   constructor(
     private toast: NbToastrService,
-    private fb: FormBuilder,
     private service: GroupService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
